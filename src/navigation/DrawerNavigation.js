1bb6362af0b8b5fd8a947da513fec3b577d33cec
Quentin Atuobi <99990675+midiiiwo@users.noreply.github.com>
2024-10-24 13:27:52 +0000

    update1

--- src/navigation/DrawerNavigation.js
+++ src/navigation/DrawerNavigation.js
@@ -1,152 +1,28 @@
 import * as React from 'react';
+import { Button, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 import RightDrawer from './Drawer';
 
-import Analytics from '../containers/Analytics';
-import Order from '../../assets/icons/order-icon.svg';
-import Analytics_ from '../../assets/icons/analytics.svg';
-import Alert from '../../assets/icons/notifications-icon.svg';
-import Checkout from '../../assets/icons/checkout.svg';
-import Products_ from '../../assets/icons/products.svg';
-import Transactions from '../../assets/icons/transactions.svg';
-import CustomersIcon from '../../assets/icons/user.svg';
-import SendMoneyIcon from '../../assets/icons/send.svg';
-// import QuickSaleIcon from '../../assets/icons/flash.svg';
-import Header from '../components/Header';
-import OrderStack from '../navigationold/OrderStackNavigator';
-import ProductStack from '../navigationold/ProductStackNavigator';
-import TransactionStack from '../navigationold/TransactionStack';
-import CustomerStack from '../navigationold/CustomerStack';
-import CheckoutStack from '../navigationold/CheckoutStack';
-// import QuickSaleNavigator from './QuickSaleNavigator';
-import NotificationStack from '../navigationold/NotificationStack';
-import SendMoneyStack from '../navigationold/SendMoneyStack';
-import { useSelector } from 'react-redux';
+// import Settings from '../containers/Settings';
+import HomeStackNavigator from './HomeStackNavigator';
 import { Dimensions } from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
 export default function DrawerNavigation() {
-  const { user } = useSelector(state => state.auth);
   return (
     <Drawer.Navigator
       initialRouteName="Home"
       screenOptions={{
+        header: () => null,
         swipeEnabled: false,
-        drawerLabelStyle: {
-          marginLeft: -10,
-          fontFamily: 'ReadexPro-Medium',
-          fontSize: 12,
-        },
-        drawerInactiveTintColor: '#526D82',
-        drawerActiveBackgroundColor: '#3F70F4',
-        drawerActiveTintColor: '#fff',
-        drawerItemStyle: {
-          borderRadius: 10,
-          paddingHorizontal: 12,
-          paddingVertical: -10,
-          height: Dimensions.get('window').height * 0.065,
+        drawerStyle: {
+          width: Dimensions.get('window').width * 0.7,
         },
       }}
       drawerContent={props => <RightDrawer {...props} />}>
-      <Drawer.Screen
-        name="Checkout"
-        component={CheckoutStack}
-        options={{
-          drawerIcon: ({ color }) => (
-            <Checkout stroke={color} height={24} width={24} />
-          ),
-          header: () => null,
-        }}
-      />
-      {/* <Drawer.Screen
-        name="Quick Sale"
-        component={QuickSaleNavigator}
-        options={{
-          drawerIcon: ({ color }) => (
-            <QuickSaleIcon stroke={color} height={28} width={28} />
-          ),
-          header: () => null,
-        }}
-      /> */}
-      <Drawer.Screen
-        name="Orders"
-        component={OrderStack}
-        options={{
-          drawerIcon: ({ color }) => (
-            <Order stroke={color} height={24} width={24} />
-          ),
-          header: () => null,
-        }}
-      />
-
-      <Drawer.Screen
-        name="Products"
-        component={ProductStack}
-        options={{
-          drawerIcon: ({ color }) => (
-            <Products_ stroke={color} height={24} width={24} />
-          ),
-          header: () => null,
-        }}
-      />
-      {/* {user.user_merchant_group === 'Administrators' &&
-        user.user_permissions.includes('TRANMGT') &&
-        user.user_permissions.includes('MKPAYMT') &&
-        user.user_permissions.includes('MMMGT') && (
-          <Drawer.Screen
-            name="Send Money"
-            component={SendMoneyStack}
-            options={{
-              drawerIcon: ({ color }) => (
-                <SendMoneyIcon stroke={color} height={24} width={24} />
-              ),
-              header: () => null,
-            }}
-          />
-        )} */}
-      <Drawer.Screen
-        name="Transactions"
-        component={TransactionStack}
-        options={{
-          drawerIcon: ({ color }) => (
-            <Transactions stroke={color} height={24} width={24} />
-          ),
-          header: () => null,
-        }}
-      />
-      <Drawer.Screen
-        name="Notifications"
-        component={NotificationStack}
-        options={{
-          drawerIcon: ({ color }) => (
-            <Alert stroke={color} height={24} width={24} />
-          ),
-          header: () => null,
-        }}
-      />
-      <Drawer.Screen
-        name="Customers"
-        component={CustomerStack}
-        options={{
-          drawerIcon: ({ color }) => (
-            <CustomersIcon stroke={color} height={24} width={24} />
-          ),
-          header: () => null,
-        }}
-      />
-      <Drawer.Screen
-        name="Insights"
-        component={Analytics}
-        options={{
-          drawerIcon: ({ color }) => (
-            <Analytics_ stroke={color} height={24} width={24} />
-          ),
-          header: ({ navigation }) => (
-            <Header navigation={navigation} backgroundColor="#fff" />
-          ),
-        }}
-      />
+      <Drawer.Screen name="HomeDrawer" component={HomeStackNavigator} />
     </Drawer.Navigator>
   );
 }
